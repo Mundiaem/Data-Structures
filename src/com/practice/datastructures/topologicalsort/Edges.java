c6bd84ab4b4f2efd4e4ec94e71d267556ef44496
Dominic Mundia <dominicmundia@Dominic-Mundia-Kweli.local>
2022-09-13 22:52:17 +0300

    graph

---
+++ src/com/practice/datastructures/topologicalsort/Edges.java
@@ -0,0 +1,13 @@
+package com.practice.datastructures.topologicalsort;
+
+public class Edges {
+int src, dest, weight;
+
+	Edges(int src, int dest, int weight)
+	{
+		this.src= src;
+		this.dest= dest; 
+		this.weight= weight;
+}
+	
+}
---
+++ src/com/practice/datastructures/topologicalsort/TopologicalSort.java
@@ -0,0 +1,5 @@
+package com.practice.datastructures.topologicalsort;
+
+public class TopologicalSort {
+
+}
