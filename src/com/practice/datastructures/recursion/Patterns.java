a925cceb2a7c564b56f8d1bceb56f0b925b94c53
Mundiaem <machariamundia@gmail.com>
2022-12-05 09:06:53 +0300

    count sum of subsequence

--- src/com/practice/datastructures/recursion/Patterns.java
+++ src/com/practice/datastructures/recursion/Patterns.java
@@ -1,7 +1,6 @@
 package com.practice.datastructures.recursion;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -18,11 +17,13 @@ public class Patterns {
         int sum = 2;
         List<Integer> ds = new ArrayList<>();
         subsequenceOfSumK(0, ds, 0, sum, arr, n);
+        System.out.println("Only one subsequence :  " + firstSubsequenceOfSumK(0, ds, 0, sum, arr, n));
+        System.out.println("Count the number of subsequences :  " + countSubsequenceOfSumK(0, 0, sum, arr, n));
     }
 
     private static void subsequenceOfSumK(int index, List<Integer> ds, int s, int sum, int[] arr, int n) {
         if (index == n) {
-            if(s==sum){
+            if (s == sum) {
                 System.out.println(ds.toString());
             }
 
@@ -35,6 +36,47 @@ public class Patterns {
         ds.remove(ds.size() - 1);
         subsequenceOfSumK(index + 1, ds, s, sum, arr, n);
 
+
+    }
+
+    private static boolean firstSubsequenceOfSumK(int index, List<Integer> ds, int s, int sum, int[] arr, int n) {
+        if (index == n) {
+            if (s == sum) {
+                System.out.println(ds.toString());
+                return true;
+            }
+
+            return false;
+        }
+        ds.add(arr[index]);
+        s += arr[index];
+        if (firstSubsequenceOfSumK(index + 1, ds, s, sum, arr, n)) return true;
+        s -= arr[index];
+        ds.remove(ds.size() - 1);
+        if (firstSubsequenceOfSumK(index + 1, ds, s, sum, arr, n)) return true;
+        return false;
+
+
+    }
+
+    private static int countSubsequenceOfSumK(int index, int s, int sum, int[] arr, int n) {
+        // condition not satisfied
+        // strictly if the array contains positive only
+        if(s>sum)return 0;
+        if (index == n) {
+            //condition satisfied
+            if (s == sum) return 1;
+            // condition not satisfied
+            else return 0;
+
+
+        }
+        s += arr[index];
+        int l = countSubsequenceOfSumK(index + 1, s, sum, arr, n);
+        s -= arr[index];
+        // condition not met ? not picked 
+        int r = countSubsequenceOfSumK(index + 1, s, sum, arr, n);
+        return l + r;
 
     }
 }
