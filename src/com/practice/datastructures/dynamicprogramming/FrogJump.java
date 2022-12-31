b6484667612a648cd496535b5002827c32bb39bd
Mundiaem <machariamundia@gmail.com>
2022-12-31 16:37:08 +0300

    converting recursion to dp

--- src/com/practice/datastructures/dynamicprogramming/FrogJump.java
+++ src/com/practice/datastructures/dynamicprogramming/FrogJump.java
@@ -1,5 +1,7 @@
 package com.practice.datastructures.dynamicprogramming;
 
+import java.util.Arrays;
+
 /**
  * created with love by mundiaem
  * created on 31/12/2022
@@ -9,17 +11,22 @@ package com.practice.datastructures.dynamicprogramming;
 
 public class FrogJump {
     public static void main(String[] args) {
-        int [] heights={4,8,3,10,4,4};
+        int[] heights = {4, 8, 3, 10, 4, 4};
         System.out.println(frogJump(6, heights));
     }
-   static int f(int index, int [] heights){
-        if(index==0) return 0;
-        int left =f(index-1, heights)+Math.abs(heights[index]-heights[index-1]);
-                int right= Integer.MAX_VALUE;
-        if(index>1) right= f(index-2, heights)+Math.abs(heights[index]-heights[index-2]);
-        return Math.min(left, right);
+
+    static int f(int index, int[] heights, int[] dp) {
+        if (index == 0) return 0;
+        if(dp[index]!=-1) return dp[index];
+        int left = f(index - 1, heights, dp) + Math.abs(heights[index] - heights[index - 1]);
+        int right = Integer.MAX_VALUE;
+        if (index > 1) right = f(index - 2, heights, dp) + Math.abs(heights[index] - heights[index - 2]);
+        return dp[index] = Math.min(left, right);
     }
-   static int frogJump(int n, int[] heights){
-        return f(n-1, heights);
+
+    static int frogJump(int n, int[] heights) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return f(n - 1, heights, dp);
     }
 }
