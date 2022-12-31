9cca9abbe05e08dad300947c94cb60f572f4ee08
Mundiaem <machariamundia@gmail.com>
2022-12-31 16:55:05 +0300

    frog jump after dp tabulation

--- src/com/practice/datastructures/dynamicprogramming/FrogJump.java
+++ src/com/practice/datastructures/dynamicprogramming/FrogJump.java
@@ -26,7 +26,14 @@ public class FrogJump {
 
     static int frogJump(int n, int[] heights) {
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return f(n - 1, heights, dp);
+        Arrays.fill(dp, 0);
+//        return f(n - 1, heights, dp);
+        for (int i=1; i<n; i++){
+            int fs=dp[i-1]+Math.abs(heights[i]- heights[i-1]);
+            int ss =Integer.MAX_VALUE;
+            if(i>1) ss=dp[i-2]+Math.abs(heights[i]-heights[i-2]);
+            dp[i] =Math.min(fs,ss);
+        }
+        return dp[n-1];
     }
 }
