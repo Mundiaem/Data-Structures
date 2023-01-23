80e901314a8473c384887a08d78f54dbc0f6f105
Mundiaem <machariamundia@gmail.com>
2023-01-23 14:43:44 +0300

    PalindromePartitionII

--- src/com/practice/datastructures/dynamicprogramming/PalindromePartitionII.java
+++ src/com/practice/datastructures/dynamicprogramming/PalindromePartitionII.java
@@ -13,14 +13,21 @@ public class PalindromePartitionII {
      *      return 0;
      * }
      * temp="";
+     * if(dp[i]!=-1) return;
      * int minCost = Integer.MAX_VALUE;
      * for(j=i; j<n; j++){
      * temp += s[j];
      * if(isPalindrome(partition)){
      * cost =1+ f(j+1);
      * minCost= min(minCost, cost);
+     * return dp[i]=minCost;
      * }
      * }
      * }
+     * @Time Complexity
+     * - Memoization
+     *   > dp[i]=minCost;
+     *   if(dp[i]!=-1) return;
+     *
     * */
 }
